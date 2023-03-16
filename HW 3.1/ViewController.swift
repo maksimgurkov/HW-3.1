3cd98b18f6b1291a059fcc560aa132c42b0fe2bd
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-03-16 09:10:20 +0300

    Initial Commit

---
+++ HW 3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 3.1
+//
+//  Created by Максим Гурков on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
